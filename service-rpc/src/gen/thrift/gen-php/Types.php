e0e90af5b876f967dd8496953606f742f48f0099
Bharathkrishna Guruvayoor Murali <bharath@cloudera.com>
2018-06-01 05:46:54 -0500

    HIVE-14388: Add number of rows inserted message after insert command in Beeline (Bharathkrishna Guruvayoor Murali, reviewed by Sahil Takiar, Peter Vary)

--- service-rpc/src/gen/thrift/gen-php/Types.php
+++ service-rpc/src/gen/thrift/gen-php/Types.php
@@ -8443,6 +8443,10 @@ class TGetOperationStatusResp {
    * @var \TProgressUpdateResp
    */
   public $progressUpdateResponse = null;
+  /**
+   * @var int
+   */
+  public $numModifiedRows = null;
 
   public function __construct($vals=null) {
     if (!isset(self::$_TSPEC)) {
@@ -8489,6 +8493,10 @@ class TGetOperationStatusResp {
           'type' => TType::STRUCT,
           'class' => '\TProgressUpdateResp',
           ),
+        11 => array(
+          'var' => 'numModifiedRows',
+          'type' => TType::I64,
+          ),
         );
     }
     if (is_array($vals)) {
@@ -8521,6 +8529,9 @@ class TGetOperationStatusResp {
       }
       if (isset($vals['progressUpdateResponse'])) {
         $this->progressUpdateResponse = $vals['progressUpdateResponse'];
+      }
+      if (isset($vals['numModifiedRows'])) {
+        $this->numModifiedRows = $vals['numModifiedRows'];
       }
     }
   }
@@ -8616,6 +8627,13 @@ class TGetOperationStatusResp {
             $xfer += $input->skip($ftype);
           }
           break;
+        case 11:
+          if ($ftype == TType::I64) {
+            $xfer += $input->readI64($this->numModifiedRows);
+          } else {
+            $xfer += $input->skip($ftype);
+          }
+          break;
         default:
           $xfer += $input->skip($ftype);
           break;
@@ -8683,6 +8701,11 @@ class TGetOperationStatusResp {
       }
       $xfer += $output->writeFieldBegin('progressUpdateResponse', TType::STRUCT, 10);
       $xfer += $this->progressUpdateResponse->write($output);
+      $xfer += $output->writeFieldEnd();
+    }
+    if ($this->numModifiedRows !== null) {
+      $xfer += $output->writeFieldBegin('numModifiedRows', TType::I64, 11);
+      $xfer += $output->writeI64($this->numModifiedRows);
       $xfer += $output->writeFieldEnd();
     }
     $xfer += $output->writeFieldStop();
