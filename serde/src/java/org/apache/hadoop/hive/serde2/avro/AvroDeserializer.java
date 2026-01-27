964e2091cdbfa874980901e5e39df88dcdbcd1c3
Hamid Ghasemirahni <56080980+hamidgh09@users.noreply.github.com>
2026-01-27 15:20:35 +0100

    HOPSFS-316: Fix issues with avro update (#69)

--- serde/src/java/org/apache/hadoop/hive/serde2/avro/AvroDeserializer.java
+++ serde/src/java/org/apache/hadoop/hive/serde2/avro/AvroDeserializer.java
@@ -251,7 +251,7 @@ class AvroDeserializer {
 
       int scale = 0;
       try {
-        scale = fileSchema.getJsonProp(AvroSerDe.AVRO_PROP_SCALE).asInt();
+        scale = ((Number) fileSchema.getObjectProp(AvroSerDe.AVRO_PROP_SCALE)).intValue();
       } catch(Exception ex) {
         throw new AvroSerdeException("Failed to obtain scale value from file schema: " + fileSchema, ex);
       }
@@ -267,7 +267,7 @@ class AvroDeserializer {
 
       int maxLength = 0;
       try {
-        maxLength = fileSchema.getJsonProp(AvroSerDe.AVRO_PROP_MAX_LENGTH).getValueAsInt();
+        maxLength = ((Number) fileSchema.getObjectProp(AvroSerDe.AVRO_PROP_MAX_LENGTH)).intValue();
       } catch (Exception ex) {
         throw new AvroSerdeException("Failed to obtain maxLength value for char field from file schema: " + fileSchema, ex);
       }
@@ -282,7 +282,7 @@ class AvroDeserializer {
 
       maxLength = 0;
       try {
-        maxLength = fileSchema.getJsonProp(AvroSerDe.AVRO_PROP_MAX_LENGTH).getValueAsInt();
+        maxLength = ((Number) fileSchema.getObjectProp(AvroSerDe.AVRO_PROP_MAX_LENGTH)).intValue();
       } catch (Exception ex) {
         throw new AvroSerdeException("Failed to obtain maxLength value for varchar field from file schema: " + fileSchema, ex);
       }
